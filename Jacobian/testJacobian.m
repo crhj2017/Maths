4fb513f3775aaba2b3403fc67ed38d767d99e4a0
Charles Jewson <32522730+crhj2017@users.noreply.github.com>
2023-05-29 17:00:27 +0100

    Update testJacobian.m
    Improved readability

--- Jacobian/testJacobian.m
+++ Jacobian/testJacobian.m
@@ -1,11 +1,14 @@
 format long g
 format compact
 
-% Test
-f = @(x)[sin(x(3))+sin(x(1)*x(2)); cos(x(1)+x(2)*x(3)^2)];
 h = 1e-6;
+
+% Test Function
+f = @(x)[sin(x(3))+sin(x(1)*x(2)); 
+         cos(x(1)+x(2)*x(3)^2)];
 
 x0=[0;0;0];
 df0=MyJacobian(f,x0,h)
+
 x1=[1;1;1];
 df1=MyJacobian(f,x1,h)
